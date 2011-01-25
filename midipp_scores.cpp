c18eebb0d943ce040189eea6e1ba41f07e7f4f97
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2011-01-25 13:48:01 +0000

    midipp: Add support for more labels.

--- midipp_scores.cpp
+++ midipp_scores.cpp
@@ -1297,7 +1297,8 @@ MppScoreMain :: handleScoreFileSaveAs()
 int
 MppScoreMain :: checkLabelJump(int pos)
 {
-	if ((pos < 0) || (pos > 7) || (jumpTable[pos] == 0))
+	if ((pos < 0) || (pos > 12) || (pos > MPP_MAX_LABELS) ||
+	    (jumpTable[pos] == 0))
 		return (0);
 
 	return (1);
