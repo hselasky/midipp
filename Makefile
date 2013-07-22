2d87bf495cb10a0caf98b260cd2200cb1a5980c0
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2013-07-22 17:32:23 +0000

    midipp: Bump version number.

--- Makefile
+++ Makefile
@@ -26,7 +26,7 @@
 # Makefile for MIDI Player Pro
 #
 
-VERSION=1.0.25
+VERSION=1.1.0
 
 PREFIX?=/usr/local
 
--- midipp_mainwindow.cpp
+++ midipp_mainwindow.cpp
@@ -602,7 +602,7 @@ MppMainWindow :: MppMainWindow(QWidget *parent)
 
 	MidiInit();
 
-	version = tr("MIDI Player Pro v1.0.25");
+	version = tr("MIDI Player Pro v1.1.0");
 
 	setWindowTitle(version);
 	setWindowIcon(QIcon(QString(MPP_ICON_FILE)));
