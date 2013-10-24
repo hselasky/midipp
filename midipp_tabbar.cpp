ef7bf189438a20dcbb0d04b4b33582a1ba3103f8
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2013-10-24 17:31:13 +0000

    midipp: Set correct widget parent.

--- midipp_tabbar.cpp
+++ midipp_tabbar.cpp
@@ -28,8 +28,8 @@
 MppTabBar :: MppTabBar(QWidget *parent)
     : QWidget(parent)
 {
-	right_sw = new QStackedWidget();
-	left_sw = new QStackedWidget();
+	right_sw = new QStackedWidget(parent);
+	left_sw = new QStackedWidget(parent);
 
 	left_sw->setVisible(0);
 	right_sw->setVisible(1);
